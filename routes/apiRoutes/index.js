6eeb2e3c44ed89af7e66f53a6cb5b4fda97ba2b1
Blake Georgii <blakegeorgii@gmail.com>
2022-01-22 13:37:35 -0800

    added front end functionality

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -7,8 +7,31 @@ router.get('/notes', (req, res) => {
     res.json(notes);
 });
 
+router.get('/notes:id', (req, res) => {
+    res.json(notes);
+});
+
+router.delete('/notes/:id', (req, res) => {
+    let newNotes = notes;
+
+    indexOfNote = newNotes.findIndex(function name(x) {
+        return x.id == req.params.id;
+    });
+
+    newNotes.splice(indexOfNote, 1);
+
+    fs.writeFileSync(
+        path.join(__dirname, "../../db/db.json"),
+        JSON.stringify(newNotes)
+    );
+
+    res.json(notes);
+});
+
 router.post('/notes', (req, res) => {
+    req.body.id = Math.floor(Math.random() * 1000000);
     notes.push(req.body);
+
     fs.writeFileSync(
         path.join(__dirname, "../../db/db.json"),
         JSON.stringify(notes)
